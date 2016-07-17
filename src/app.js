e95b4b7ecd6704352a0d70b3ceed75d7d365b5f9
Sang Tran <tony_633@hotmail.com>
2016-07-17 11:47:52 -0400

    just need to refacter

--- src/app.js
+++ src/app.js
@@ -1,35 +1,43 @@
-var express = require('express');
-var parser = require('body-parser');
-var mongoose = require('mongoose');
-var app = express();
-var urlEncodedParser = parser.urlencoded({extended:false});
+var express          		= require('express');
+var parser           		= require('body-parser');
+var mongoose         		= require('mongoose');
+var app              		= express();
+//add ./ when same directory as app.js
+var Campground      		= require('./models/campground');
+var Comment         		= require('./models/comment'); 
+var User             		= require('./models/user'); 
+var seedDB           		= require('./seeds');
+var passport 				= require('passport');
+var localStrategy 			= require('passport-local');
+var passportLocalMongoose 	= require('passport-local-mongoose'); 
+
 
 mongoose.connect('mongodb://localhost/yelp_camp');
 
 app.set('view engine', 'jade');
+app.use(express.static('public'));
+app.use(parser.urlencoded({extended:true}));
+seedDB();
 
-var campgroundSchema = new mongoose.Schema({
-	name:String,
-	image:String,
-	description:String
-});
+//Passport configuration
+app.use(require('express-session')({
+	secret:'this is secret',
+	resave:false,
+	saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-var Campground = mongoose.model('Campground', campgroundSchema);
+app.use(function(req,res,next){
+	//locals let req.user be available to all routes
+	res.locals.currentUser = req.user;
+	next();
+})
 
-// Campground.create({
-// 	name:"Campsite 2",
-// 	image:'https://farm4.staticflickr.com/3162/2642197987_2c71947286.jpg',
-// 	description:"This is a decscription for lala"
-// }, function(err,campground){
-// 	if(err){
-// 		console.log(err);
-// 	} else {
-// 		console.log('newly created campground');
-// 		console.log(campground);
-// 	}
-// })
-
-
+//ROUTES
 app.get('/',function(req,res){
 	res.render('landing');
 })
@@ -37,19 +45,21 @@ app.get('/',function(req,res){
 
 //Index - show all campgrounds
 app.get('/campgrounds',function(req,res){
+	console.log(req.user);
+
 	//get data from db
 	Campground.find({},function(err,allCampgrounds){
 		if(err){
 			console.log(err);
 		} else {
 			console.log('got the data');
-			res.render('index', {campgrounds:allCampgrounds});
+			res.render('campgrounds/index', {campgrounds:allCampgrounds});
 		}
 	})
 });
 
 //Create -add new campgrounds
-app.post('/campgrounds', urlEncodedParser, function(req,res){
+app.post('/campgrounds', function(req,res){
 	//var newCampGround = req.body;
 	//campgrounds.push(newCampGround);
 	Campground.create({
@@ -69,20 +79,118 @@ app.post('/campgrounds', urlEncodedParser, function(req,res){
 
 //NEW - show form to create new campground
 app.get('/campgrounds/new',function(req,res){
-	res.render('new');
+	res.render('campgrounds/new');
 })
 
 //SHOW specific campground
 app.get('/campgrounds/:id', function(req,res){
 	var id = req.params.id;
-	Campground.findById(id, function(err,foundCampground){
+	Campground.findById(id).populate('comments').exec(function(err,foundCampground){
 		if(err){
 			console.log(err);
 		} else {
-			res.render('show',{campground:foundCampground});
+			console.log(foundCampground);
+			res.render('campgrounds/show',{campground:foundCampground});
 		}
 	})	
 })
+
+
+
+//===============
+// COMMENTS ROUTES
+//===============
+
+app.get('/campgrounds/:id/comments/new', isLoggedIn, function(req,res){
+	var id = req.params.id;
+	Campground.findById(id, function(err, foundCampground){
+		if(err){
+			console.log(err);
+		} else {
+			console.log(foundCampground);
+			res.render('comments/new',{campground:foundCampground});
+		}
+	})
+});
+
+//POST NEW COMMENTS
+//TODO add date and time of new comment
+app.post('/campgrounds/:id/comments', isLoggedIn,function(req,res){
+	//lookup campground using ID
+	//create new comment
+	//connect new comment to campground
+	//redirect to campground show page
+	var id = req.params.id;
+	Campground.findById(id, function(err, foundCampground){
+		if(err){
+			console.log(err);
+			redirect('/campgrounds');
+		} else {
+			console.log(req.body);
+			Comment.create({
+				text : req.body.text,
+				author:req.body.author
+			}, function(err,comment){
+				if(err){
+					console.log(err);
+				} else {
+					foundCampground.comments.push(comment);
+					foundCampground.save();
+					res.redirect('/campgrounds/' + foundCampground._id);
+				}
+			})
+		}
+	})
+})
+
+//AUTH ROUTES
+//=============
+
+app.get('/register', function(req,res){
+	res.render('register');
+})
+//REGISTER ROUTES 
+app.post('/register', function(req,res){
+	var newUser = new User({
+		username:req.body.username
+	})
+	User.register(newUser, req.body.password, function(err,user){
+		if(err){
+			console.log(err);
+			return res.render('register');
+		} 
+		passport.authenticate('local')(req,res,function(){
+			res.redirect('/campgrounds');
+		})
+	})
+})
+
+//LOGIN ROUTES
+app.get('/login', function(req,res){
+	res.render('login');
+})
+
+//handle login logic
+app.post('/login', passport.authenticate('local', {
+	successRedirect: '/campgrounds',
+	failureRedirect: '/login'
+}), function(req,res){
+
+})
+
+//LOGOUT ROUTE
+app.get('/logout', function(req,res){
+	req.logout();
+	res.redirect('/campgrounds');
+})
+
+function isLoggedIn(req,res,next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		res.redirect('/login');
+	}
+}
 
 app.listen(process.env.PORT || 3000, function(){
 	console.log('listening to port 3000');
---
+++ src/seeds.js
@@ -0,0 +1,67 @@
+var mongoose   = require('mongoose');
+var Campground = require('./models/campground');
+var Comment    = require('./models/comment');
+var data = [
+	{
+		name:"Canyon's Death", 
+		image:'https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg',
+		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in metus tortor. Quisque urna arcu, molestie eget quam eget, hendrerit consequat mauris. Suspendisse nec varius magna. Mauris sit amet sollicitudin risus, id egestas libero. Praesent congue commodo orci, vel tempus odio varius vel. Praesent in dolor eu enim malesuada suscipit a at sem. Aenean at dui imperdiet, mollis ante convallis, mattis nisi. Mauris at ligula vel metus imperdiet commodo.'
+	},
+	{
+		name:'Rocky IV', 
+		image:'https://farm9.staticflickr.com/8167/7121865553_e1c6a31f07.jpg',
+		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in metus tortor. Quisque urna arcu, molestie eget quam eget, hendrerit consequat mauris. Suspendisse nec varius magna. Mauris sit amet sollicitudin risus, id egestas libero. Praesent congue commodo orci, vel tempus odio varius vel. Praesent in dolor eu enim malesuada suscipit a at sem. Aenean at dui imperdiet, mollis ante convallis, mattis nisi. Mauris at ligula vel metus imperdiet commodo.'
+	},
+	{
+		name:'Jumanji', 
+		image:'https://farm4.staticflickr.com/3069/2618662727_4603c3a203.jpg',
+		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in metus tortor. Quisque urna arcu, molestie eget quam eget, hendrerit consequat mauris. Suspendisse nec varius magna. Mauris sit amet sollicitudin risus, id egestas libero. Praesent congue commodo orci, vel tempus odio varius vel. Praesent in dolor eu enim malesuada suscipit a at sem. Aenean at dui imperdiet, mollis ante convallis, mattis nisi. Mauris at ligula vel metus imperdiet commodo.'
+	}
+
+]
+
+function seedDB() {
+	Comment.remove({},function(err){
+		if(err){
+			console.log(err);
+		} else {
+			console.log('comments removed too');
+		}
+	})
+
+	Campground.remove({},function(err){
+		if(err){
+			console.log(err);
+		} else {
+			console.log('contents removed!');
+			// add campgrounds
+			data.forEach(function(seed){
+				Campground.create(seed, function(err,campground){
+					if(err){
+						console.log(err);
+					} else {
+						console.log('added campground');
+						//create a comment
+						Comment.create({
+							text:'this place is great',
+							author:'homer'
+						},function(err,comment){
+							if(err){
+								console.log(err);
+							} else {
+								campground.comments.push(comment);
+								campground.save();
+								console.log('created new comment');
+							}
+						});
+					}
+				})
+			})
+		} //end of else
+	});
+
+
+	
+}
+
+module.exports = seedDB;
